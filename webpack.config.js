3a88c4ab07615922e11790eb3aa2067173ab7c0d
Eredost <michael.coutin62@gmail.com>
2020-06-22 14:46:20 +0200

    build: add Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'src/assets/images',
+                    to: 'images'
+                },
+                {
+                    from: 'src/assets/index.html'
+                }
+            ]
+        }),
+        new BrowserSyncPlugin({
+            files: ['src/scss/*', 'src/assets/*.html'],
+            startPath: '/public',
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost:8080/',
+        }, {
+            reload: false
+        })
+    ],
+    devServer: {
+        publicPath: '/public/',
+        stats: 'errors-only',
+        host: process.env.HOST,
+        port: process.env.PORT,
+        open: false
+    }
+};
